fbaef159ce5d67cda8468bb0fa2737dd336b908e
Abhishek Rajput <98444175+abhishekrajput6916@users.noreply.github.com>
2023-10-26 04:30:23 +0530

    Update FormA.js

--- pages/FormA.js
+++ pages/FormA.js
@@ -1,14 +1,27 @@
 'use client';
 import { useState } from 'react';
 
-function FormA({ onSubmit, age }) {
-  const handleSubmit = (event) => {};
+function FormA({ onSubmit, setStep, age }) {
+  const [show,setShow]=useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(oldData=>{
+      return {
+        ...oldData,
+        type:"FormA",
+        DS:show,
+      }
+    })
+    setStep(4);
+  };
   return (
     <form id='dc' onSubmit={handleSubmit}>
       <h2>Form A</h2>
-      <label>
+      <label> 
         Select DC Shows:
-        <select>
+        <select onChange={(e)=>{
+          setShow(e.target.value);
+        }}>
           <option value=''>--Select--</option>
           <option value='The Flash'>The Flash</option>
           <option value='Arrow'>Arrow</option>
